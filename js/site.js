739ebd7a239cf302227670f45ea67a3cbb0151e6
Jerry McKee Jr <JerryMcKeeJr.Dev@gmail.com>
2023-10-13 12:29:37 -0400

    set up sudo code and debuged getValues

--- js/site.js
+++ js/site.js
@@ -1,12 +1,41 @@
-function displayMessage() {
-    // get the text out of input and assign to a variable
-    let msg = document.getElementById('message').value;
-    
-    Swal.fire(
-        {
-            backdrop: false,
-            title: 'FizzBuzz',
-            text: msg, // <-- replace this with the variable
-        }
-    );
+// from site.js
+
+// Get the numbers from the page
+function getValues(){
+
+    // Create the variables
+    let startValue = document.getElementById('startValue').value;
+    let endValue = document.getElementById('endValue').value;
+
+    // convert text to integers
+    startValue = parseInt(startValue);
+    endValue = parseInt(endValue);
+
+    // Validate the user input
+    if (Number.isInteger(startValue) && Number.isInteger(endValue) && startValue < endValue){
+
+    }
+    else
+    {
+        Swal.fire(
+            {
+                icon: 'error',
+                backdrop: false,
+                title: 'Oops',
+                text: 'Please enter valid numbers only. Ensure Starting value is less than the Ending Value.'
+            });
+    }
+
+    // test the connection to the form
+}
+
+// generate the list of numbers
+function generateListOfNumbers(){
+
+}
+
+
+// display the list of numbers
+function displayListOfNumbers(){
+
 }
